ff7eb70fa227ec8b3277ca5c72e4876160a64ddf
Michiel ter Reehorst <michiel@swydo.com>
2017-01-12 09:42:47 +0100

    Split tests for moment extension and PreviousDateRange

--- test/previous-date-range.spec.js
+++ test/previous-date-range.spec.js
@@ -1,18 +1,8 @@
 /* eslint-env mocha, chai */
 /* eslint-disable no-var, vars-on-top, func-names, prefer-arrow-callback, no-unused-expressions */
 var expect = require('chai').expect;
-var moment = require('moment');
 
 var PreviousDateRange = require('../lib/previous-date-range').default;
-
-describe('moment.fn.previous', function () {
-  it('should return a range', function () {
-    var range = moment().previous(2, 'weeks');
-
-    expect(moment.isMoment(range.start)).to.be.ok;
-    expect(moment.isMoment(range.end)).to.be.ok;
-  });
-});
 
 describe('PreviousDateRange', function () {
   beforeEach(function () {
