6f874cfa6551434aa593881411210a6dd7756b0e
Michiel ter Reehorst <michiel@swydo.com>
2018-12-10 09:54:45 +0100

    Implement `#locale` and pass locale to formatters

--- src/format.js
+++ src/format.js
@@ -1,7 +1,19 @@
 // @flow
 import moment from 'moment';
-import RelativeRange from './relative-range';
 import type { FormatStaticOptionsType } from './relative-range';
+
+export type RelativeRangeInput = {
+  count: number;
+  measure: string;
+  locale?: string;
+}
+
+export type StaticRangeInput = {
+  date?: moment.Moment | string;
+  start: moment.Moment;
+  end: moment.Moment;
+  locale?: string;
+}
 
 export const DEFAULT_STATIC_RANGE_LOCALE = {
   separator: '-',
@@ -38,7 +50,7 @@ moment.updateLocale('en', {
 });
 
 const formatStatic = (
-    range: RelativeRange,
+    range: StaticRangeInput,
     format: string = 'll',
     options: FormatStaticOptionsType = {},
 ) => {
@@ -46,12 +58,15 @@ const formatStatic = (
       date,
       start,
       end,
+      locale = moment.locale(),
     } = range;
+  const localMoment = moment().locale(locale);
+
   const {
       separator = '-',
       otherYear: otherYearConfig = '%s YYYY',
       // eslint-disable-next-line no-underscore-dangle
-    } = moment.localeData()._config.staticRange || DEFAULT_STATIC_RANGE_LOCALE;
+  } = localMoment.localeData()._config.staticRange || DEFAULT_STATIC_RANGE_LOCALE;
   const {
       attemptYearHiding = false,
   } = options;
@@ -63,14 +78,14 @@ const formatStatic = (
   const sameDay = sameMonth && start.date() === end.date();
   const isReadable = format.toLowerCase() === 'll';
   const monthsMergable = sameMonth && !sameDay && isReadable;
-  const longDateFormat = moment.localeData().longDateFormat(format);
+  const longDateFormat = localMoment.localeData().longDateFormat(format);
   const longMonthFormat = isReadable ? longDateFormat.replace(/([^MD.]*)YYYY([^MD.]*)/, '') : longDateFormat;
   const year = date ? moment(date).year() : moment().year();
   const startThisYear = start.year() === year;
   const endThisYear = end.year() === year;
 
-  start.locale(moment.locale());
-  end.locale(moment.locale());
+  start.locale(locale);
+  end.locale(locale);
 
   let startFormat;
   let endFormat;
@@ -112,20 +127,25 @@ const relativeTypeByCount = (count) => {
   }
 };
 
-const formatRelative = (range: RelativeRange, format: string = 'RR') => {
+const formatRelative = (
+  range: RelativeRangeInput,
+  format: string = 'RR',
+) => {
   const {
       count,
-      countableMeasure: measure,
+      measure,
+      locale = moment.locale(),
     } = range;
+  const localMoment = moment().locale(locale);
 
   // eslint-disable-next-line no-underscore-dangle
-  let locale = moment.localeData()._config.relativeRange || DEFAULT_RELATIVE_RANGE_LOCALE;
-  locale = { ...locale, ...locale[format] };
+  let localeData = localMoment.localeData()._config.relativeRange || DEFAULT_RELATIVE_RANGE_LOCALE;
+  localeData = { ...localeData, ...localeData[format] };
 
   const type = relativeTypeByCount(count);
 
   const countKey = [measure, count].join('_');
-  const countTranslation = locale[countKey];
+  const countTranslation = localeData[countKey];
 
   if (countTranslation) {
     return countTranslation.replace('%s', Math.abs(count));
@@ -134,12 +154,12 @@ const formatRelative = (range: RelativeRange, format: string = 'RR') => {
   const absCount = Math.abs(count);
   const plural = absCount > 1;
   const pluralKey = plural ? `${measure}_plural` : measure;
-  const translation = locale[pluralKey] || locale[measure] || '';
+  const translation = localeData[pluralKey] || localeData[measure] || '';
 
   const typePluralKey = plural ? `${type}_plural` : type;
-  const typeTranslation = locale[`${type}_${measure}`] || locale[`${type}_${absCount}`] || locale[typePluralKey] || locale[type] || '';
+  const typeTranslation = localeData[`${type}_${measure}`] || localeData[`${type}_${absCount}`] || localeData[typePluralKey] || localeData[type] || '';
 
-  return typeTranslation.replace('%d', translation.replace('%s', absCount));
+  return typeTranslation.replace('%d', translation.replace('%s', `${absCount}`));
 };
 
 export {
--- src/relative-range.js
+++ src/relative-range.js
@@ -108,6 +108,7 @@ export type RelativeRangeOptionsType = {
   start?: moment.Moment;
   end?: moment.Moment;
   minimumStart?: moment.Moment;
+  locale?: string;
 }
 
 export type RelativeRangeJsonOptionsType = {
@@ -308,13 +309,45 @@ class RelativeRange {
     return parts.every((key: RangePartEnum) => this.data[key]);
   }
 
+  locale(locale?: string | boolean): string | RelativeRange {
+    if (typeof locale === 'undefined') {
+      const localeLocale: string = this.data.locale || moment.locale();
+      return localeLocale;
+    }
+    if (typeof locale === 'boolean') {
+      if (locale === false) {
+        delete this.data.locale;
+      }
+      return this;
+    }
+    this.data.locale = locale;
+
+    return this;
+  }
+
   format(format?: string, options?: FormatStaticOptionsType): string {
+    const localLocale = this.locale();
+    let locale;
+
+    if (typeof localLocale === 'string') {
+      locale = localLocale;
+    }
+
     switch (format) {
       case 'r':
       case 'R':
-        return formatRelative(this, format);
+        return formatRelative({
+          count: this.count,
+          measure: this.countableMeasure,
+          locale,
+        }, format);
       default:
-        return formatStatic(this, format, options);
+        return formatStatic({
+          date: this.date,
+          start: this.start,
+          end: this.end,
+          locale,
+        }, format, options);
     }
   }
 
